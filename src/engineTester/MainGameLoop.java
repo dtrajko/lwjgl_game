5b717a14385e58df3c7f2b38f14cecb759abd030
Dejan Trajkovic <dtrajko@gmail.com>
2018-01-17 04:10:53 +0100

    View matrix / camera

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,6 +3,7 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
 import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
@@ -19,37 +20,96 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 
 		// OpenGL expects vertices to be defined counter clockwise by default
-		float[] vertices = {
-			// Left bottom triangle
-			-0.5f,  0.5f, 0f, // V0
-			-0.5f, -0.5f, 0f, // V1
-			 0.5f, -0.5f, 0f, // V2
-			 0.5f,  0.5f, 0f, // V3
+		float[] vertices = {			
+			-0.5f,0.5f,-0.5f,	
+			-0.5f,-0.5f,-0.5f,	
+			0.5f,-0.5f,-0.5f,	
+			0.5f,0.5f,-0.5f,		
+			
+			-0.5f,0.5f,0.5f,	
+			-0.5f,-0.5f,0.5f,	
+			0.5f,-0.5f,0.5f,	
+			0.5f,0.5f,0.5f,
+			
+			0.5f,0.5f,-0.5f,	
+			0.5f,-0.5f,-0.5f,	
+			0.5f,-0.5f,0.5f,	
+			0.5f,0.5f,0.5f,
+			
+			-0.5f,0.5f,-0.5f,	
+			-0.5f,-0.5f,-0.5f,	
+			-0.5f,-0.5f,0.5f,	
+			-0.5f,0.5f,0.5f,
+			
+			-0.5f,0.5f,0.5f,
+			-0.5f,0.5f,-0.5f,
+			0.5f,0.5f,-0.5f,
+			0.5f,0.5f,0.5f,
+			
+			-0.5f,-0.5f,0.5f,
+			-0.5f,-0.5f,-0.5f,
+			0.5f,-0.5f,-0.5f,
+			0.5f,-0.5f,0.5f
+		};
+		
+		float[] textureCoords = {	
+			0,0,
+			0,1,
+			1,1,
+			1,0,			
+			0,0,
+			0,1,
+			1,1,
+			1,0,			
+			0,0,
+			0,1,
+			1,1,
+			1,0,
+			0,0,
+			0,1,
+			1,1,
+			1,0,
+			0,0,
+			0,1,
+			1,1,
+			1,0,
+			0,0,
+			0,1,
+			1,1,
+			1,0,		
 		};
 		
 		int[] indices = {
-				0, 1, 3, // Top left triangle (V0, V1, V3)
-				3, 1, 2, // Bottom right triangle (V3, V1, V2)
-		};
-		
-		float[] textureCoords = {
-				0, 0, // V0
-				0, 1, // V1
-				1, 1, // V2
-				1, 0, // V3
+			0,1,3,	
+			3,1,2,	
+			4,5,7,
+			7,5,6,
+			8,9,11,
+			11,9,10,
+			12,13,15,
+			15,13,14,	
+			16,17,19,
+			19,17,18,
+			20,21,23,
+			23,21,22
 		};
 
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("tiles")));
-		Entity entity = new Entity(staticModel, new Vector3f(-0.3f, 0, 0), 0, 0, 0, 1);
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("frame")));
+		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		Camera camera = new Camera();
+
 		while(!Display.isCloseRequested()) {
-			// game logic
+			// entity.increasePosition(0, 0, 0);
+			entity.increaseRotation(0, 1, 1);
+			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadViewMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -1,5 +1,6 @@
 package renderEngine;
 
+import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
@@ -14,9 +15,23 @@ import toolbox.Maths;
 
 public class Renderer {
 
+	private static final float FOV = 70; // field of view angle
+	private static final float NEAR_PLANE = 0.1f;
+	private static final float FAR_PLANE = 1000;
+
+	private Matrix4f projectionMatrix;
+
+	public Renderer(StaticShader shader) {
+		createProjectionMatrix();
+		shader.start();
+		shader.loadProjectionMatrix(projectionMatrix);
+		shader.stop();
+	}
+
 	public void prepare() {
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		GL11.glClearColor(0, 0.5f, 0.9f, 1);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		GL11.glClearColor(0.2f, 0.2f, 0.2f, 0.5f);
 	}
 
 	public void render(Entity entity, StaticShader shader) {
@@ -33,5 +48,20 @@ public class Renderer {
 		GL20.glDisableVertexAttribArray(0);
 		GL20.glDisableVertexAttribArray(1);
 		GL30.glBindVertexArray(0);
+	}
+	
+	private void createProjectionMatrix() {
+		float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
+		float y_scale = (float) ((1f / Math.tan(Math.toRadians(FOV / 2f))) * aspectRatio);
+		float x_scale = y_scale / aspectRatio;
+		float frustum_length = FAR_PLANE - NEAR_PLANE;
+		
+		projectionMatrix = new Matrix4f();
+		projectionMatrix.m00 = x_scale;
+		projectionMatrix.m11 = y_scale;
+		projectionMatrix.m22 = -((FAR_PLANE + NEAR_PLANE) / frustum_length);
+		projectionMatrix.m23 = -1;
+		projectionMatrix.m32 = -((2 * FAR_PLANE * NEAR_PLANE) / frustum_length);;
+		projectionMatrix.m33 = 0;
 	}
 }
